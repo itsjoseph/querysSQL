92157c30bcdb60a8151125f12ea564c217a639c6
conor <conor@localhost.localdomain>
2025-06-30 23:23:55 -0600

    Actualizacion de tabla usuarios

--- tablas/usuarios.sql
+++ tablas/usuarios.sql
@@ -1,10 +1,14 @@
 drop table if EXISTS usuario;
-CREATE TABLE usuario (
-    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    rol ENUM('ADMIN', 'VENDEDOR', 'SUPERVISOR') NOT NULL,
-    activo BOOLEAN DEFAULT TRUE,
-    fecha_creacion date DEFAULT CURRENT_DATE
-);
+CREATE TABLE `Usuarios` (
+  `id_usuario` varchar(100) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `apellido_paterno` varchar(50) DEFAULT NULL,
+  `apellido_materno` varchar(50) DEFAULT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `rol` enum('ADMIN','VENDEDOR','SUPERVISOR') NOT NULL,
+  `activo` tinyint(1) DEFAULT 1,
+  `fecha_creacion` date DEFAULT curdate(),
+  PRIMARY KEY (`id_usuario`),
+  UNIQUE KEY `username` (`username`)
+) 
