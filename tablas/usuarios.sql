dedf150659321f6f3f63fe39eb8e74b4c097605c
conor <conor@localhost.localdomain>
2025-08-24 20:46:42 -0600

    Actualizacion de campos en Usuarios

--- tablas/usuarios.sql
+++ tablas/usuarios.sql
@@ -1,4 +1,5 @@
-drop table if EXISTS usuario;
+DROP TABLE IF EXISTS `Usuarios`;
+
 CREATE TABLE `Usuarios` (
   `id_usuario` varchar(100) NOT NULL,
   `nombre` varchar(100) NOT NULL,
@@ -9,6 +10,8 @@ CREATE TABLE `Usuarios` (
   `rol` enum('ADMIN','VENDEDOR','SUPERVISOR') NOT NULL,
   `activo` tinyint(1) DEFAULT 1,
   `fecha_creacion` date DEFAULT curdate(),
+  `fecha_actualizacion` date DEFAULT NULL,
+  `fecha_vencimiento` date DEFAULT NULL,
   PRIMARY KEY (`id_usuario`),
   UNIQUE KEY `username` (`username`)
-) 
+)
